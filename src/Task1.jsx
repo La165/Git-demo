134f1c95aa2f96fbc2c5a4f9bbb46ef99eefb19a
Lalitha Janapareddy <jlalitha2001@gmail.com>
2025-12-04 15:53:53 +0530

    updated Task1.jsx

--- src/Task1.jsx
+++ src/Task1.jsx
@@ -2,10 +2,10 @@ function Task1()
 {
         let EmpfullName = "Mr. Ratan Sir";
 
-  const product = {
+  const student = {
     id: 1001,
-    name: "Laptop",
-    brand: "Lenovo"
+    stuname: "Lalitha",
+    course: "Java Full Stack"
   };
   const fruits = ["apple", "mango", "banana"];
   let fruitsList = fruits.map((fruit, index) => <li key={index}>{fruit}</li>);
@@ -17,12 +17,11 @@ function Task1()
 
       <h1>Good Evening... {EmpfullName}</h1>
 
-      <h2>Product Details</h2>
-      <p>ID: {product.id}</p>
-      <p>Name: {product.name}</p>
-      <p>Brand: {product.brand}</p>
+      <h2>Student Details</h2>
+      <p>ID: {student.id}</p>
+      <p>Name: {student.stuname}</p>
+      <p>Course: {student.course}</p>
 
-      
       <h3>Fruits</h3>
       <ol>{fruitsList}</ol>
 
